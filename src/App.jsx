6d08809c54dbae8825974642af0e8d4e0a220204
Gean Victor <geanvictorduque@gmail.com>
2023-04-24 12:55:00 -0300

    Update css

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import './styles/global.css';
 
 const createUserFormSchema = z.object({
   name: z.string()
-    .nonempty('O nome é obrigatório')
+    .nonempty({message: 'O nome é obrigatório'})
     .transform(name => {
       return name.trim().split(' ').map(word => {
         return word[0].toLocaleUpperCase().concat(word.substring(1))
@@ -15,27 +15,30 @@ const createUserFormSchema = z.object({
     }),
 
   email: z.string()
-    .nonempty('O email é obrigatório')
-    .email('Formato de email inválido')
+    .nonempty({message: 'O email é obrigatório'})
+    .email({message: 'Formato de email inválido'})
     .refine(email => {
       return email.endsWith('@gmail.com')
-    }, 'O e-mail precisa ser gmail'),
+    }, {message: 'O e-mail precisa ser gmail'}),
 
   password: z.string()
-    .min(6, 'A senha precisa de no mínimo 6 caracteres'),
+    .min(6, {message: 'A senha precisa de no mínimo 6 caracteres'}),
 
   techs: z.array(z.object({
-    title: z.string().nonempty('O título é obrigatório'),
-    knowledge: z.coerce.number().min(1, 'O nível de conhecimento deve ser de 1 a 10').max(10)
-  })).min(2, 'Insira pelo menos 2 tecnologias')
+    title: z.string().nonempty({message: 'O título é obrigatório'}),
+    knowledge: z.coerce.number().min(1, {message: 'O nível de conhecimento deve ser de 1 a 10'}).max(10, {message: 'O nível de conhecimento deve ser de 1 a 10'})
+  })).min(2, {message: 'Insira pelo menos 2 tecnologias'})
 });
 
 function App() {
   const [output, setOutput] = useState('');
 
-  const { register, handleSubmit, control, formState: {errors} } = useForm({
-    resolver: zodResolver(createUserFormSchema),
-  });
+  const { 
+    register, 
+    handleSubmit, 
+    control, 
+    formState: {errors} 
+  } = useForm({ resolver: zodResolver(createUserFormSchema) });
 
   const { fields, append, remove} = useFieldArray({
     control,
@@ -43,15 +46,15 @@ function App() {
   });
 
   function addNewTech() {
-    append({title: '', knowledge: 0})
-  }
+    append({title: ''})
+  };
   
   function createUser(data) {
     setOutput(JSON.stringify(data,null,2))
   }
   return (
-    <main className='h-screen bg-zinc-950 text-zin-50 flex flex-col items-center justify-center'>
-      <form onSubmit={handleSubmit(createUser)} action="" className='flex flex-col gap-4 w-full max-w-xs'>
+    <main className=' text-zinc-50 flex flex-col items-center pt-10 justify-center'>
+      <form onSubmit={handleSubmit(createUser)} action="" className='flex flex-col bg-zinc-900 gap-4 w-full max-w-md p-3'>
 
         <div className='flex flex-col gap-1'>
           <label htmlFor="name">Nome</label>
@@ -75,7 +78,7 @@ function App() {
         <div className='flex flex-col gap-1'>
           <label htmlFor="password" className='flex items-center justify-between'>
             Tecnologias
-            <button type='button' onClick={addNewTech} className='text-emerald-500 text-sm bg-zinc-950'>Adicionar</button>
+            <button type='button' onClick={addNewTech} className='text-emerald-500 text-sm bg-zinc-900'>Adicionar</button>
           </label>
           
           {fields.map((field, index) => {
@@ -87,7 +90,7 @@ function App() {
                 </div>
                 
                 <div className="flex flex-col gap-1">
-                  <input type="number" className='w-16 border border-zin-200 shadow-sm rounded h-10 px-3 bg-zinc-800' placeholder='Digite o nível de conhecimento' {...register(`techs.${index}.knowledge`)} />
+                  <input type="number" className='w-full border border-zin-200 shadow-sm rounded h-10 px-3 bg-zinc-800' placeholder='Digite seu nível de skill' {...register(`techs.${index}.knowledge`)} />
                   {errors.techs?.[index]?.knowledge && <span className='text-red-500 text-sm'>{errors.techs?.[index]?.knowledge.message}</span>}
                 </div>
               </div>
@@ -98,7 +101,12 @@ function App() {
 
           <button className='bg-emerald-500 rounded font-semibold text-white h-10 hover:bg-emerald-600' type='submit'>Salvar</button>
       </form>
-      <pre>{output}</pre>
+      
+      {output && (
+        <pre className='text-sm my-5  bg-zinc-800 text-zinc-100 p-6 rounded-lg'>
+          {output}
+        </pre>
+      )}
     </main>
   )
 }
